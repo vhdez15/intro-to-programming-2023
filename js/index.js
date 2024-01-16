9d2268d2ca7cf349b363942eb67b7dde0042d7da
vhdez515 <vhdez515@gmail.com>
2024-01-16 13:34:40 -0500

    < 4.3 edits>

--- js/index.js
+++ js/index.js
@@ -12,3 +12,37 @@ let listItem = document.createElement('li');
 listItem.innerText = skills[i];
 skillsList.appendChild(listItem);
 };
+
+const messageForm = document.getElementsByName ('leave_message')[0];
+messageForm.addEventListener('submit',function(event) {
+    event.preventDefault();
+    const userName = event.target.userName.value;
+    const userEmail = event.target.userEmail.value;
+    const userMessage = event.target.userMessage.value;
+
+    console.log(`Name: ${userName}`);
+    console.log(`Email: ${userEmail}`);
+    console.log(`Message: ${userMessage}`);
+
+  const messageSection = document.getElementById('messages');
+  const messageList = messageSection.querySelector('ul');
+  const newMessage = document.createElement('li');
+  
+  newMessage.innerHTML =`
+  <a href="mailto:${userEmail}">${userName}</a>
+  <span>${userMessage}</span>
+  `;
+  
+  const removeButton = document.createElement('button');
+  removeButton.innerText = "Remove";
+  removeButton.setAttribute("type","button");
+  removeButton.addEventListener('click',function(event){
+  const entry = event.target.parentNode;
+  entry.remove();
+  
+ });
+
+ newMessage.appendChild(removeButton);
+ messageList.appendChild(newMessage); 
+   messageForm.reset();
+});
