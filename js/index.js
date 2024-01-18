e250d0f2bc185f80bca929f96840dc965b067ed7
vhdez515 <vhdez515@gmail.com>
2024-01-18 11:25:44 -0500

    4.4 styling my page

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ const today = new Date();
 const thisYear = today.getFullYear();
 const footer = document.querySelector('footer');
 const copyright = document.createElement ('p');
-copyright.innerHTML = ' Vale Hdez' + thisYear;
+copyright.innerHTML = ' Vale Hdez'+ ' ' + thisYear;
 footer.appendChild(copyright);
 const skills = ['fluent in spanish','Works well with others','Adaptable','JavaScript'];
 const skillsSection = document.getElementById('skills');
